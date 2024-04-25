2796827334ca3b77eaf0569eedb4d5f226c20aa2
Hal's Laptop <halrobisoncreative@gmail.com>
2024-04-24 21:30:30 -0500

    query parameters chapter examples

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -2,13 +2,14 @@ package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
     // Handles request at /hello
-    @GetMapping("hello")
+    @GetMapping("helloSpring")
     @ResponseBody
     public String hello() {
         return "Hello, Spring!";
@@ -21,4 +22,10 @@ public class HelloController {
         return "Goodbye, Spring!";
     }
 
+    // Handles requests of the form /hello?name=(printName)
+    @GetMapping("hello")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello, " + name + "!";
+    }
 }
