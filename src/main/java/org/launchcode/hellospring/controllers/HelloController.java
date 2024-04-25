a84f2fcf6cd43a651d0398e66c47803446b1f59c
Hal's Laptop <halrobisoncreative@gmail.com>
2024-04-24 22:27:43 -0500

    forms with controllers chapter examples

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,10 +1,7 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
@@ -23,8 +20,9 @@ public class HelloController {
         return "Goodbye, Spring!";
     }
 
-    // Dynamic - Handles requests of the form /hello?name=(printName)
-    @GetMapping("hello")
+    // Dynamic - Handles requests of the form /hello?name=(name that will print)
+    // Updated to RequestMapping - which allows GET and POST request.
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
     @ResponseBody
     public String helloWithQueryParam(@RequestParam String name) {
         return "Hello, " + name + "!";
@@ -37,4 +35,32 @@ public class HelloController {
         return "Hello, " + name + "!";
     }
 
+    @GetMapping("form")
+    @ResponseBody
+    public String helloForm() {
+        return "<html>" +
+                "<body>" +
+                "<form action='hello'>" + // tells the form to submit a request to /hello (which already works with helloWithQueryParam
+                "<input type='text' name='name'>" +
+                "<input type='submit' value='Greet me!'>" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+        // note that by default, this form is submitted with a GET request!
+        // so, the GetMapping annotations in QueryParam work with this.
+    }
+
+    @GetMapping("postForm")
+    @ResponseBody
+    public String helloPostForm() {
+        return "<html>" +
+                "<body>" +
+                "<form action='hello' method='post'>" + // tells the form to submit a request to /hello (which already works with helloWithQueryParam
+                "<input type='text' name='name'>" +
+                "<input type='submit' value='Greet me!'>" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+        // updated method to post request!
+    }
 }
