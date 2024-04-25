de2536609ceec9954e0dca58de0bc06fc7d5ce57
Hal's Laptop <halrobisoncreative@gmail.com>
2024-04-24 21:52:48 -0500

    path variable chapter examples

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -2,30 +2,39 @@ package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
-    // Handles request at /hello
+    // Static - Handles request at /hello
     @GetMapping("helloSpring")
     @ResponseBody
     public String hello() {
         return "Hello, Spring!";
     }
 
-    // Handles request at /goodbye
+    // Static - Handles request at /goodbye
     @GetMapping("goodbye")
     @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
     }
 
-    // Handles requests of the form /hello?name=(printName)
+    // Dynamic - Handles requests of the form /hello?name=(printName)
     @GetMapping("hello")
     @ResponseBody
     public String helloWithQueryParam(@RequestParam String name) {
         return "Hello, " + name + "!";
     }
+
+    // Handles requests of the form /helloPath/LaunchCode
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloWithPathParam(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+
 }
