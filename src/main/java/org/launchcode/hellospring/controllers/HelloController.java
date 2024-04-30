c7d2cd9d07e38b80f6ddde3fba8c410f75a0de6c
Hal's Laptop <halrobisoncreative@gmail.com>
2024-04-29 21:24:05 -0500

    static form page with thymeleaf template

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -6,68 +6,21 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class HelloController {
 
-    @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
     @ResponseBody
-    public String hello(@RequestParam String name, @RequestParam String helloTranslated) {
-        return helloTranslated + name + "!";
+    public String helloWithQueryParam(@RequestParam String name){
+        return "Hello, " + name + "!";
     }
 
-    // Handles requests at /hello/languages
-    @GetMapping("languages")
+    //handle requests of the form http://localhost:8080/hello/LaunchCode
+    @GetMapping("hello/{name}")
     @ResponseBody
-    public String createMessage() {
-        return "<html>" +
-                    "<body>" +
-                        "<form action='hello' method='post'>" + // tells the form to submit a request to /hello (which already works with helloWithQueryParam
-                            "<input type='text' name='name'>" +
-                            "<label for='language-select'>Choose a language!</label>" +
-                                "<select name='createMessage' id='language-select'" +
-                                "<option value=''>--Please choose a language--</option>" +
-                                "<option value='Hello, '>English</option>" +
-                                "<option value='Bonjour, '>French</option>" +
-                                "<option value='Hola, '>Spanish</option>" +
-                                "<option value='Hallo, '>German</option>" +
-                                "<option value='Dia dhuit, '>Irish</option>" +
-                            "<input type='submit' value='Greet me!'>" +
-                        "</form>" +
-                    "</body>" +
-                "</html>";
+    public String helloWithPathParam(@PathVariable String name){
+        return "Hello, " + name + "!";
+    }
 
-
-        // for notes: cleaner solution posted by LaunchCode:
-//        @RequestMapping(value="hello", method = RequestMethod.POST)
-//        @ResponseBody
-//        public String helloPost(@RequestParam String name, @RequestParam String language) {
-//            if (name == null) {
-//                name = "World";
-//            }
-//
-//            return createMessage(name, language);
-//
-//            // For a bonus mission, students can change this response text to look nicer.
-//            // This is subjective, but students should be modifying the HTML of the response string.
-//        }
-//
-//        public static String createMessage(String n, String l) {
-//            String greeting = "";
-//
-//            if (l.equals("english")) {
-//                greeting = "Hello";
-//            }
-//            else if (l.equals("french")) {
-//                greeting = "Bonjour";
-//            }
-//            else if (l.equals("italian")) {
-//                greeting = "Bonjourno";
-//            }
-//            else if (l.equals("spanish")) {
-//                greeting = "Hola";
-//            }
-//            else if (l.equals("german")) {
-//                greeting = "Hallo";
-//            }
-//
-//            return greeting + " " + n;
-//        }
+    @GetMapping("form")
+    public String helloForm() {
+        return "form";
     }
 }
